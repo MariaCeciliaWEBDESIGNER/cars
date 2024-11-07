c9874a1364318937a24f6fd0834dcea8d34b9253
Clarinha <mariaclaraemariaceciliagemeas@gmail.com>
2024-11-07 09:13:19 -0300

    Criado modelo Car e adicionado ao admin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=50, null=False)
+    brand = models.CharField(max_length=50, null=False)
+    year = models.IntegerField(max_length=50, null=False)
+    filename = models.CharField(max_length=50, null=False)
+
+
+
